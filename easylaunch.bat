7120f9d085e8be519a8e9aa41efd9beaf4ede23a
linktlh <linktlh@gmail.com>
2018-09-22 20:53:53 -0500

    Allow to be downloaded and launched
    I extracted all the files, split phase 3.5 as it was too big for upload. also added a fresh launch script for easy launching.

---
+++ easylaunch.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+set TTI_PLAYCOOKIE=usernameTruthfullyDoesntMatterBcOfHowTTINowWorks
+set TTI_GAMESERVER=gameserverIPAlsoDosentMatterBcOfHowTTIWorks
+:a
+infinite.exe
+echo .
+echo .
+echo .
+echo Looks like you crashed!
+echo to return to TTI hit any key.
+pause
+goto a
